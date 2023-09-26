82cc740c7894a3f438e74dbb92a0567cf6194e0e
niswith <mohamed55mouden@gmail.com>
2023-09-26 22:52:22 +0100

    add strecture of the project with pages ejs

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//This tells Express to serve any files from the 'public' folder. 
+app.use(express.static('public'));
+
+// Définir le moteur de vues EJS
+app.set('view engine', 'ejs');
+
+// Définir le répertoire des vues
+app.set('views', './views'); 
+
+
+// the main page
+app.get('/',(req,res)=>{
+    res.send('this is the main page !');
+})
+
+// the register page
+app.get('/register',(req,res)=>{
+    res.render('register');
+
+})
+// the login page 
+app.get('/login',(req,res)=>{
+    res.render('login');
+
+})
+// the layout page 
+app.get('/layout',(req,res)=>{
+    res.render('layout');
+
+})
+// the dashboard page 
+app.get('/dashboard',(req,res)=>{
+    res.render('dashboard');
+
+})
+
+// start of the serveur
+app.listen(PORT,()=>{
+    console.log("Server don start  http://localhost:"+PORT);
+} )
